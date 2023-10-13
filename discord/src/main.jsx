ddd100a7d72bf48c4f7670900401fbaa21bc057a
Francisco Acedo <franciscoa@devanddel.com>
2023-10-13 11:13:01 +0200

    Fix develop :(

--- discord/src/main.jsx
+++ discord/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import Modal from 'react-modal';
 import './index.css'
 import { UserContextProvider } from './context/UserContext.jsx'
 
