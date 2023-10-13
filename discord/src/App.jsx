45b9d0eeba9f078add95e6fda3d6f82221fee893
OscarRomaCalvo <oscar.roma.calvo@alumnos.upm.es>
2023-10-13 11:03:56 +0200

    WIP on Card

--- discord/src/App.jsx
+++ discord/src/App.jsx
@@ -2,15 +2,13 @@ import { useState, useContext } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { UserContext } from './context/UserContext'
+import Card from './components/Card/Card'
 
 
 function App() {
-
-  const {user, changeState} = useContext(UserContext) 
-
   return (
     <>
+      <Card/>
     </>
   )
 }
