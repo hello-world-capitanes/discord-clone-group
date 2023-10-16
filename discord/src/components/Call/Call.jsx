a76122cff072bf850307632165b09e10bdb393d4
BABEL-SI\david.almeda <david.almeda@babelgroup.com>
2023-10-16 10:07:15 +0200

    Terminado el call

--- discord/src/components/Call/Call.jsx
+++ discord/src/components/Call/Call.jsx
@@ -1,12 +1,4 @@
-import { useState } from 'react'
 import './index.css'
-import Image from '../Image/Image'
-import Button from '../Button/Button'
-import GifLogo from '../../assets/film.svg'
-import GiftLogo from '../../assets/gift.svg'
-import addLogo from '../../assets/plus-circle.svg'
-import emojiLogo from '../../assets/smile.svg'
-import imageLogo from '../../assets/image.svg'
 import CallFriends from './CallFriends/CallFriends'
 import CallChat from './CallChat/CallChat'
 function Call({ friends }) {
