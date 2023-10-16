bde499cdeca6c58ce0ee011d2bdfde67fea348a1
Alesaanchez <alejandra.sanchez.atienza@gmail.com>
2023-10-16 11:01:46 +0200

    cabecera-inicio

---
+++ discord/src/components/cabecera-inicio/cabecera.jsx
@@ -0,0 +1,18 @@
+
+import './cabecera.css'
+import lupaImagen from './search.svg'
+
+
+function Header () {
+    return (
+        <div className="header">
+            <div className="content">
+                <h1> Encuentra tu comunidad en Discord </h1>
+                <p> Desde juegos, a música y enseñanza, aquí encontrarás tu sitio</p>
+                <input type="text" placeholder="Explora comunidades" className="search-input" />
+            </div>
+         </div>
+    )
+}
+
+export default Header 
