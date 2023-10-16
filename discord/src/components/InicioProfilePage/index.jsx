6d6f284fc51fc517dd78a2a1db96366b5152542e
OscarRomaCalvo <oscar.roma.calvo@alumnos.upm.es>
2023-10-16 12:15:30 +0200

    Integración hecha, fallo en estilos

--- discord/src/components/InicioProfilePage/index.jsx
+++ discord/src/components/InicioProfilePage/index.jsx
@@ -1,13 +1,15 @@
+import {useState} from 'react'
 import CabeceraInicio from "../cabecera-inicio/cabecera"
 import CardList from "../CardList/CardList"
-
+import './index.css'
 export const Incio =()=>{
 
+    const [filtro, setFiltro] = useState("")
 
     return (
-        <>
-            <CabeceraInicio/>
-            <CardList/>
-        </>
+        <div className="body">
+            <CabeceraInicio setFiltro={setFiltro} filtro={filtro}/>
+            <CardList filtro={filtro}/>
+        </div>
     )
 }
